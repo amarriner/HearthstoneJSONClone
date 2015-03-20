e0f51348b0502149984c9a897faa8ae1648fbe41
amarriner <amarriner@amarriner.com>
2015-03-20 14:14:40 -0400

    Adjusting prettify to move files to subdirectories

--- prettify.py
+++ prettify.py
@@ -21,12 +21,18 @@ for file in glob.glob('json/*'):
    if not os.path.isdir('xml'):
       os.mkdir('xml')
 
-   f = codecs.open('xml/' + '.'.join(file.split('/')[1].split('.')[:-1]) + '.xml', 'w', encoding='utf-8')
+   if not os.path.isdir('xml/' + file.split('.')[1]):
+      os.mkdir('xml/' + file.split('.')[1])
+
+   f = codecs.open('xml/' + file.split('.')[1] + '/' + '.'.join(file.split('/')[-1].split('.')[:-1]) + '.xml', 'w', encoding='utf-8')
    f.write(soup.prettify())
    f.close()
 
    print 'Prettifying JSON ...'
 
-   f = codecs.open(file, 'w', encoding='utf-8')
+   if not os.path.isdir('json/' + file.split('.')[1]):
+      os.mkdir('json/' + file.split('.')[1])
+
+   f = codecs.open('json/' + file.split('.')[1] + '/' + file.replace('json/', "") , 'w', encoding='utf-8')
    f.write(unicode(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))))
    f.close()
