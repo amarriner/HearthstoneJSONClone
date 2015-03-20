23a8b9906e7af1d2c270752954e98afc7bfcfcf9
amarriner <amarriner@amarriner.com>
2015-03-20 14:34:58 -0400

    Comments

--- prettify.py
+++ prettify.py
@@ -8,13 +8,17 @@ import glob
 import json
 import os
 
+# Loop through each available language in the input directory and create
+# an 'All' file containing all of the sets for the given langauge
 for language in glob.glob('input/Basic*'):
    language = language.split('.')[1]
 
-   print 'Creating all JSON for ' + language
+   print 'Creating All JSON for ' + language
 
    j = {}
 
+   # Loop through each file for the given language and add it to the transient 
+   # JSON object
    for file in glob.glob('input/*.' + language + '.json'):
       f = codecs.open(file, 'r', encoding='utf-8')
       cards = json.loads(f.read())
@@ -22,36 +26,45 @@ for language in glob.glob('input/Basic*'):
 
       j[file.replace('input/', "").split('.')[0]] = cards
 
+   # Write the JSON file to disk
    f = codecs.open('input/All.' + language + '.json', 'w', encoding='utf-8')
    f.write(unicode(json.dumps(j), 'utf-8'))
    f.close()
       
-
+# Loop through all the JSON files in the input directory for processing
 for file in glob.glob('input/*'):
+
+   # Convert to XML
    print 'Converting ' + file + ' to xml ...'
 
    f = codecs.open(file, 'r', encoding='utf-8')
    j = json.loads(f.read())
    f.close()
 
+   # Prettify the XML
    xml = dicttoxml.dicttoxml(j, custom_root="cards", attr_type=False)
    soup = BeautifulSoup(xml)
 
+   # Create directories if they're not there
    if not os.path.isdir('xml'):
       os.mkdir('xml')
 
    if not os.path.isdir('xml/' + file.split('.')[1]):
       os.mkdir('xml/' + file.split('.')[1])
 
+   # Write the XML file to disk
    f = codecs.open('xml/' + file.split('.')[1] + '/' + '.'.join(file.split('/')[-1].split('.')[:-1]) + '.xml', 'w', encoding='utf-8')
    f.write(soup.prettify())
    f.close()
 
+   # Process the JSON file itself 
    print 'Prettifying JSON ...'
 
+   # Create directories if necessary
    if not os.path.isdir('json/' + file.split('.')[1]):
       os.mkdir('json/' + file.split('.')[1])
 
+   # Write file to disk
    f = codecs.open('json/' + file.split('.')[1] + '/' + file.replace('input/', "") , 'w', encoding='utf-8')
    f.write(unicode(json.dumps(j, sort_keys=True, indent=4, separators=(',', ': '))))
    f.close()
